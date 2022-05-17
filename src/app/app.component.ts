e13ef2a53127aa47dab1aed51524974ed35b3f64
lacaprinamini <frank.capria2000@gmail.com>
2022-05-17 19:09:53 +0200

    2 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component, VERSION } from '@angular/core';
 
+class teatro {
+ file: number;
+ posti: number;
+ constructor( file: number, posti: number) {
+  postiSala= Array(file).fill("").map(() => Array(posti).fill("x")),
+}
+}
+
+
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class ordinePrenotazione  {
+  plateaPrenotazione: Array<teatro>= = new teatro(7,10),;
+  
+  constructor{
+creaTeatro
+
+
+this.prenotazione
+
+
+
+  }
+
+
 }

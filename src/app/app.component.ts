0d219cb61c4032a5cf0813fc54203a2a39544448
lacaprinamini <frank.capria2000@gmail.com>
2022-05-18 20:15:21 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,27 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  platea: Array<any>;
+  platea: Array<string>;
   
   constructor(){
 
     const nfilePlatea = 7;
     const npostiPlatea = 10;
-
-    this.platea= Array(nfilePlatea).fill("").map(() => Array(npostiPlatea).fill("x"));
+    const array=[];
+    this.platea= array;
     
+    const plateaPosti= Array(nfilePlatea).fill("").map(() => Array(npostiPlatea).fill("x"));
 
-
-
-
+   plateaPosti.map((fila, i) => {
+      var p = fila.map((nome, j) => {
+          
+          const posto="P"+(j+1)+(i+1);
+           array.push(posto);
+           
+      });
+     
+    });
+    
   }
-
-
+  
 }

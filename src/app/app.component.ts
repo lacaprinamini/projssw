38dd15236db00341cbeea1baec74e0aaa35d3a7b
lacaprinamini <frank.capria2000@gmail.com>
2022-05-23 18:43:21 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,18 +43,6 @@ const teatro = {
     this.platea= new posti(teatro.platea);
     this.palchi= new posti(teatro.palchi);
   }
-//   change(posto: string,colore: string){   
-  
-//    let i=0;
-//    colore=="green" ? this.color="red" : this.color="green";
-  
-//        var  trovato= this.platea.postiDisponibili.filter(
-//           el => ( el === posto )
-          
-//           );
-          
-  
-// }
 
 
 

4877df1ada90a1f93d153c4971643814d28330c2
lacaprinamini <frank.capria2000@gmail.com>
2022-05-23 19:03:04 +0200

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,37 +13,32 @@ class posti {
     this.postiDisponibili= posti;
   }
 }
-
-
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-
   platea: posti;
   palchi: posti;
   color: "";
-  
   constructor(){
-
 const n = {
 filePlatea: 7,
 postiPlatea: 10,    
 filePalchi: 4,
 postiPalchi: 6
 }
-
 const teatro = {
   platea: Array(n.filePlatea).fill("").map(() => Array(n.postiPlatea).fill("x")),
   palchi: Array(n.filePalchi).fill("").map(() => Array(n.postiPalchi).fill("x")),
 };
-
     this.platea= new posti(teatro.platea);
     this.palchi= new posti(teatro.palchi);
   }
-
-
-
+    imposta(posto: string){   
+  
+   
+   
+       }
 }
--- src/app/color.directive.ts
+++ src/app/color.directive.ts
@@ -4,18 +4,14 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
   selector: '[appColor]'
 })
 export class colorDirective {
-
   constructor(private el: ElementRef) { }
-
   @Input() defaultColor = '';
-
+  
   @Input() appColor= '';
-
+  
   @HostListener('click') onClick() {
     this.highlight(this.appColor || this.defaultColor || 'red');
   }
-
-
   private highlight(color: string) {
     this.el.nativeElement.style.color = color;
   }

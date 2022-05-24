9669131d44659debd7ea7ee06941d46d413e0010
lacaprinamini <frank.capria2000@gmail.com>
2022-05-24 21:07:03 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,20 @@ import { Component, VERSION } from '@angular/core';
 class posti {
   postiDisponibili: Array<string>;
   constructor(postiDisponibili: Array<any>) {
-    let posti= [];
+    let posti= [];let obj1 = {}; 
     postiDisponibili.map((fila, i) => {
         fila.map((nome, j) => {  
           const posto="P"+(j+1)+(i+1);
-           posti.push(posto);         
+           posti.push(posto);  
+           obj1[j] = [posto];        
       });
+      
+
+
+
+for (let key of Object.keys(obj1)) { 
+  console.log(key + " => " + obj1[key] ) 
+} 
     });
     this.postiDisponibili= posti;
   }
@@ -23,6 +31,7 @@ export class AppComponent  {
   palchi: posti;
   color: "";
   constructor(){
+   
 const n = {
 filePlatea: 7,
 postiPlatea: 10,    
@@ -33,6 +42,8 @@ const teatro = {
   platea: Array(n.filePlatea).fill("").map(() => Array(n.postiPlatea).fill("x")),
   palchi: Array(n.filePalchi).fill("").map(() => Array(n.postiPalchi).fill("x")),
 };
+
+
     this.platea= new posti(teatro.platea);
     this.palchi= new posti(teatro.palchi);
   }
